f6635ca2a651e3085f00e0854b3a92e2ed871a10
Ali Alsarraf <ali.alsarraf@csu.fullerton.edu>
2024-10-06 10:59:50 +0300

    submit

---
+++ main.dart
@@ -0,0 +1,32 @@
+void main() {
+  final customer = Customer("Ali");
+  customer.add(54.5);
+  customer.add(12.2);
+  print(customer.getPurchaseAmount());
+  final loyalCustomer = LoyalCustomer("Ali");
+  loyalCustomer.add(54.5);
+  loyalCustomer.add(12.2);
+  print(loyalCustomer.getPurchaseAmount());
+}
+
+class Customer {
+  String name;
+  double _purchaseAmount = 0;
+
+  Customer(this.name);
+
+  void add(double price) {
+    _purchaseAmount += price;
+  }
+
+  double getPurchaseAmount() {
+    return _purchaseAmount;
+  }
+}
+
+class LoyalCustomer extends Customer {
+  LoyalCustomer(super.name);
+  @override
+  // TODO: implement _purchaseAmount
+  double get _purchaseAmount => (super._purchaseAmount * 0.9);
+}
